bde5a1d12e783d22781e8078172b7f62872a330d
clevmania <ernestcleverest@gmail.com>
2019-04-21 07:06:45 +0100

    Initial commit

---
+++ app/src/main/java/com/clevmania/moviemageek/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.clevmania.moviemageek
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
