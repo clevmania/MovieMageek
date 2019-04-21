93ffaf28941a9f6d5066ffe3d9eba65d9447789a
clevmania <ernestcleverest@gmail.com>
2019-04-21 08:06:33 +0100

    wired pager

--- app/src/main/java/com/clevmania/moviemageek/MainActivity.kt
+++ app/src/main/java/com/clevmania/moviemageek/MainActivity.kt
@@ -2,6 +2,7 @@ package com.clevmania.moviemageek
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.clevmania.moviemageek.adapter.SliderAdapter
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -16,5 +17,6 @@ class MainActivity : AppCompatActivity() {
     private fun setupSlidePager() {
         val movieSlides : ArrayList<SlideModel> = arrayListOf()
         movieSlides.add(SlideModel(R.drawable.ic_launcher_background,"Launcher"))
+        v_slide_pager.adapter = SliderAdapter(this, movieSlides)
     }
 }
