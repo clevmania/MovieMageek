3362bca30819436e77142d1308d0129e20e70066
clevmania <ernestcleverest@gmail.com>
2019-04-22 03:41:01 +0100

    added some movie images

--- app/src/main/java/com/clevmania/moviemageek/MainActivity.kt
+++ app/src/main/java/com/clevmania/moviemageek/MainActivity.kt
@@ -16,7 +16,12 @@ class MainActivity : AppCompatActivity() {
 
     private fun setupSlidePager() {
         val movieSlides : ArrayList<SlideModel> = arrayListOf()
-        movieSlides.add(SlideModel(R.drawable.ic_launcher_background,"Launcher"))
+        movieSlides.add(SlideModel(R.drawable.arrow,"Arrow"))
+        movieSlides.add(SlideModel(R.drawable.blind_spot,"Blind Spot"))
+        movieSlides.add(SlideModel(R.drawable.got,"Game of Thrones"))
+        movieSlides.add(SlideModel(R.drawable.supernatural,"Supernatural"))
+        movieSlides.add(SlideModel(R.drawable.the_orville,"The Orville"))
+        movieSlides.add(SlideModel(R.drawable.the_walking_dead,"The Walking Dead"))
         v_slide_pager.adapter = SliderAdapter(this, movieSlides)
     }
 }
