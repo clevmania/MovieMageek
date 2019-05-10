1eec718ca28a026673434bc1862c1e07273f9c1e
clevmania <ernestcleverest@gmail.com>
2019-05-10 21:04:27 +0100

    created movie model

---
+++ app/src/main/java/com/clevmania/moviemageek/data/model/MovieModel.kt
@@ -0,0 +1,4 @@
+package com.clevmania.moviemageek.data.model
+
+data class MovieModel(val title : String, val description : String, val thumbnail : String,
+                      val studio : String, val rating : String, val streamLink : String)
