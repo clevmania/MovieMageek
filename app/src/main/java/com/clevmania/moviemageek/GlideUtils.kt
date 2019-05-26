24c6b1f7df091dcfb873a33d112ef9a207451eae
clevmania <ernestcleverest@gmail.com>
2019-05-26 23:10:41 +0100

    wrap up detailed view design

---
+++ app/src/main/java/com/clevmania/moviemageek/GlideUtils.kt
@@ -0,0 +1,7 @@
+package com.clevmania.moviemageek
+
+import com.bumptech.glide.annotation.GlideModule
+import com.bumptech.glide.module.AppGlideModule
+
+@GlideModule
+class GlideUtils : AppGlideModule()
--- app/src/main/java/com/clevmania/moviemageek/MovieDetailActivity.kt
+++ app/src/main/java/com/clevmania/moviemageek/MovieDetailActivity.kt
@@ -2,6 +2,7 @@ package com.clevmania.moviemageek
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.animation.AnimationUtils
 import kotlinx.android.synthetic.main.activity_movie_detail.*
 
 class MovieDetailActivity : AppCompatActivity() {
@@ -10,6 +11,11 @@ class MovieDetailActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_movie_detail)
 
-        iv_detailed_movie_thumbnail.setImageResource(intent.getIntExtra("extra_url",0))
+        GlideApp.with(this).load(intent.getIntExtra("extra_url",0))
+            .into(iv_detailed_movie_thumbnail)
+        tv_movie_title.text = intent.getStringExtra("extra_title")
+        supportActionBar?.title = intent.getStringExtra("extra_title")
+        iv_movie_cover.animation = AnimationUtils.loadAnimation(this,R.anim.scale_anim)
+        fab_play_detail.animation = AnimationUtils.loadAnimation(this,R.anim.scale_anim)
     }
 }
